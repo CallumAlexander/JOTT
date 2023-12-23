7828abd114fa5a14af9067190265f1cc8caf59b4
Callum Alexander <callumalexander.personal@gmail.com>
2023-12-23 12:09:06 +0000

    Exclude subdirectories from the random link selection by only extracting [A-Z0-9]/[^/]+\.html

--- JOTT.sh
+++ JOTT.sh
@@ -7,7 +7,8 @@ echo "Courtesy of The on-line hacker Jargon File, version 4.4.7"
 BASE_URL="http://www.catb.org/jargon/html/"
 URL="http://www.catb.org/jargon/html/go01.html"
 
-LINKS=$(timeout 2s curl -s "$URL" | grep -o '<a [^>]*href="[^"]*"[^>]*>' | sed 's/<a [^>]*href="\([^"]*\)"[^>]*>/\1/g')
+LINKS=$(timeout 2s curl -s "$URL" | grep -o '<a [^>]*href="[^"]*"[^>]*>' | sed 's/<a [^>]*href="\([^"]*\)"[^>]*>/\1/g' | grep -E '[A-Z0-9]/[^/]+\.html')
+
 
 if [ -z "$LINKS" ]; then
     echo "No links found on the webpage."
@@ -19,6 +20,7 @@ IFS=$'\n' read -rd '' -a LINK_ARRAY <<< "$LINKS"
 TOTAL_LINKS="${#LINK_ARRAY[@]}"
 RANDOM_INDEX=$((RANDOM % TOTAL_LINKS))
 RANDOM_LINK="${LINK_ARRAY[RANDOM_INDEX]}"
+echo $RANDOM_LINK
 FULL_URL="$BASE_URL$RANDOM_LINK"
 
 # echo "$FULL_URL"
@@ -38,16 +40,20 @@ DEFINITION=$(echo "$DEFINITION" | sed 's/&#8221;/"/g')
 DEFINITION=$(echo "$DEFINITION" | sed 's/&#8216;/'\''/g')
 DEFINITION=$(echo "$DEFINITION" | sed 's/&#8217;/'\''/g')
 
+DEFINITION=$(echo "$DEFINITION" | sed 's/&#8212;/'—'/g')
+
 DEFINITION=$(echo "$DEFINITION" | sed 's/<i class="glossterm">\(.*\)<\/i>/\1/g')
 DEFINITION=$(echo "$DEFINITION" | sed 's/<i class="citetitle">\(.*\)<\/i>/\1/g')
 
 # DEFINITION=$(echo "$DEFINITION" | sed 's/<span[^>]*>\(.*\)<\/span>/\1/g')
-DEFINITION=$(echo "$DEFINITION" | sed 's/<span class="quote">\(.*\)<\/span>/\1/g')
-DEFINITION=$(echo "$DEFINITION" | sed 's/<span class="emphasis">\(.*\)<\/span>/\1/g')
-DEFINITION=$(echo "$DEFINITION" | sed 's/<span class="citerefentry">\(.*\)<\/span>/\1/g')
-DEFINITION=$(echo "$DEFINITION" | sed 's/<span class="firstterm">\(.*\)<\/span>/\1/g')
-DEFINITION=$(echo "$DEFINITION" | sed 's/<span[^>]*>\(.*\)<\/span>/\1/g')
+# DEFINITION=$(echo "$DEFINITION" | sed 's/<span class="quote">\(.*\)<\/span>/\1/g')
+# DEFINITION=$(echo "$DEFINITION" | sed 's/<span class="emphasis">\(.*\)<\/span>/\1/g')
+# DEFINITION=$(echo "$DEFINITION" | sed 's/<span class="citerefentry">\(.*\)<\/span>/\1/g')
+# DEFINITION=$(echo "$DEFINITION" | sed 's/<span class="firstterm">\(.*\)<\/span>/\1/g')
+# DEFINITION=$(echo "$DEFINITION" | sed 's/<span[^>]*>\(.*\)<\/span>/\1/g')
 
+
+DEFINITION=$(echo "$DEFINITION" | sed 's/<span[^>]*>//g; s/<\/span>//g')
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Jargon of the Terminal (JOTT)
 
 Author : _Callum Alexander_ (TheCatThatBarks)
+
 Name credits mainly but not completely : _Callum Groeger_ (groegercesg)
 
 ### To-do list of things to to
@@ -10,9 +11,14 @@ Name credits mainly but not completely : _Callum Groeger_ (groegercesg)
 - [ ] Option for `n` number of random definition requests
 - [ ] Punctuation and Grammar inclusion if applicable for the term
 - [ ] Handling when the source is down
+- [X] Exclude subdirectories in the random search
 - [ ] Specify the request timeout - default is 2 seconds currently
 - [ ] Fix handling and removal of `<i>` tags
 - [ ] Fix handling and removal of `<span>` tags
 - [ ] Ability to click and follow redirects and links
 - [ ] Install script/command that adds it to your MOTD or terminal rc file
+- [ ] Handle bold tags like this command class: `<b class="command">Unable to kill all processes.  Blast them (y/n)?</b>`
+- [ ] Handle italic tags like this: `<em>which</em>`
+- [ ] Handle italic tags like this: `<i class="citetitle">A Stress Analysis Of A Strapless Evening
+   Gown: And Other Essays For A Scientific Eye</i>`
 
